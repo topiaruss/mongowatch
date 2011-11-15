a91f29a809fb281541fdfa80146681aa3792fa0d
Russ Ferriday <russf@topia.com>
2011-11-14 22:51:44 -0800

    initial creation, derived from Stephan Richter's mongopersist layout

---
+++ setup.py
@@ -0,0 +1,39 @@
+"""Setup
+"""
+import os
+from setuptools import setup, find_packages
+
+setup (
+    name='mongowatch',
+    version='0.1.0dev',
+    author = "Russ Ferriday",
+    author_email = "russf@topia.com",
+    description = "MongoDB traffic pattern watcher",
+    license = "ZPL 2.1",
+    keywords = "mongo testing",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Framework :: ZODB',
+        'License :: OSI Approved :: Zope Public License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent'],
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+    extras_require = dict(
+        test = (
+            'zope.app.testing',
+            'zope.testing',
+            ),
+        zope = (
+            ),
+        ),
+    install_requires = [
+        'pymongo',
+        'setuptools',
+    ],
+    include_package_data = True,
+    zip_safe = False,
+    )
---
+++ src/mongowatch/tests/test_watcher.py
@@ -0,0 +1,35 @@
+##############################################################################
+#
+# Copyright (c) 2011 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Mongo Watcher Tests"""
+import doctest
+import persistent
+import pprint
+from pymongo import dbref, objectid
+
+from mongowatch import testing, mapping
+
+def doctest_MongoWatch_simple():
+    r"""MongoWatch: simple
+
+    MongoWatch provides an easy way to overview mongo database traffic::
+
+
+    """
+
+
+def test_suite():
+    return doctest.DocTestSuite(
+        setUp=testing.setUp, tearDown=testing.tearDown,
+        checker=testing.checker,
+        optionflags=testing.OPTIONFLAGS)
